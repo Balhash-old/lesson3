726ebecc636363d5e8d093f90f5d71afbc06bc39
Balhash-old <tel-alex@mail.ru>
2024-08-12 11:27:37 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+first_name='12'
+second_name='1.5'
+third_name='Python'
+first_name='0.125 часа'
+print('Курс :',third_name,',всего задач :',first_name,',затрачено часов',second_name,',среднее время выполнения',first_name)
